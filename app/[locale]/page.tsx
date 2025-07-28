43fbdef42bd0abc7face3e0e99bca2583c66a41c
ígorpetriv.bsc <igorpetriv.bsc@gmail.com>
2025-07-29 00:14:17 +0300

    some responsive styles added toposts page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,6 +2,7 @@ import PaginationClient from "@/components/Pagination/Pagination.client";
 import { fetchAllPosts } from "@/lib/api";
 import React from "react";
 import css from "./Posts.module.css";
+import Link from "next/link";
 
 interface SearchParams {
   searchParams?: Promise<{
@@ -18,8 +19,11 @@ const Posts = async ({ searchParams }: SearchParams) => {
     <div className={css.container}>
       <ul className={css.postList}>
         {posts.map((post) => (
-          <li className={css.postCard} key={post.id}>
-            {post.title}
+          <li key={post.id} className={css.postCard}>
+            <p>{post.title}</p>
+            <Link className={css.buttonLink} key={post.id} href={"/"}>
+              <span>More</span>
+            </Link>
           </li>
         ))}
       </ul>
